4726f98b84b04a75e91e932c59fde5f86219e823
GucioWons <wgutek1@wp.pl>
2022-08-10 10:34:04 +0200

    Initial commit

---
+++ src/main/java/com/guciowons/shoppingbasket/ShoppingbasketApplication.java
@@ -0,0 +1,13 @@
+package com.guciowons.shoppingbasket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingbasketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingbasketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guciowons/shoppingbasket/ShoppingbasketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guciowons.shoppingbasket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingbasketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
